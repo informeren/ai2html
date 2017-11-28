28e32c6121123992d18db03815314dbfdaf29ced
wulff <wulff@ratatosk.net>
2017-11-28 11:40:43 +0100

    Added Typekit support

--- ai2html.js
+++ ai2html.js
@@ -3064,6 +3064,7 @@ function generateOutputHtml(content, pageName, settings) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />\
     <meta name="viewport" content="width=device-width, initial-scale=1" />\
+    <link rel="stylesheet" href="https://use.typekit.net/hhi6htw.css">\
 ';
   htmlHeader += content.head + '\r' + css + '\r' + '\n<title>ai2HTML</title>\n</head>\n<body>\n';
   var htmlFooter = '\
